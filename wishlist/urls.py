0186cf29434a214ddc96992a00faf4715c861287
sashanabilafortuna <sashanabilafortuna@gmail.com>
2022-09-22 12:53:37 +0700

    lab 3 PBP - form, authentication, session, cookie

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, send_wishlist_xml, send_wishlist_json, show_json_by_id, show_xml_by_id
+from wishlist.views import register, login_user, logout_user, show_wishlist, send_wishlist_xml, send_wishlist_json, show_json_by_id, show_xml_by_id
 
 app_name = 'wishlist'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('json/', send_wishlist_json, name='send_wishlist_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
